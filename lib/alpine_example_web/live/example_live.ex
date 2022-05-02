cbec7326f74327ff04b40ceb3d5516c539eaa027
Christopher Grainger <chris@amplified.ai>
2022-05-02 16:06:04 +1000

    add example_live

---
+++ lib/alpine_example_web/live/example_live.ex
@@ -0,0 +1,27 @@
+defmodule AlpineExampleWeb.ExampleLive do
+  use AlpineExampleWeb, :live_view
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+      <button phx-click="click">Click me</button>
+      <div>LiveView value: <%= @shadowed %></div>
+      <div x-data={"{shadowed: '#{@shadowed}'}"}>
+        Alpine value: <span x-text="shadowed"></span>
+      </div>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, shadowed: "foo")}
+  end
+
+  @impl true
+  def handle_event("click", _params, socket) do
+    {:noreply, update(socket, :shadowed, &set_shadowed/1)}
+  end
+
+  defp set_shadowed("foo"), do: "bar"
+  defp set_shadowed("bar"), do: "foo"
+end
--- lib/alpine_example_web/router.ex
+++ lib/alpine_example_web/router.ex
@@ -17,7 +17,7 @@ defmodule AlpineExampleWeb.Router do
   scope "/", AlpineExampleWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", ExampleLive, :index
   end
 
   # Other scopes may use custom stacks.
